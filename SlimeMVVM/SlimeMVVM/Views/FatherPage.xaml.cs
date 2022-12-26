7aadd71e8578e83bda57c554e6a250e57e7d002e
MigAnge2000 <18cs0307@cdserdan.tecnm.mx>
2022-12-26 15:25:46 -0600

    DemoMVVM

---
+++ SlimeMVVM/SlimeMVVM/Views/FatherPage.xaml.cs
@@ -0,0 +1,26 @@
+﻿using Rg.Plugins.Popup.Services;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+using Xamarin.Forms;
+using Xamarin.Forms.Xaml;
+
+namespace SlimeMVVM.Views
+{
+    [XamlCompilation(XamlCompilationOptions.Compile)]
+    public partial class FatherPage : ContentPage
+    {
+        public FatherPage()
+        {
+            InitializeComponent();
+        }
+
+        private void ImageButton_Clicked(object sender, EventArgs e)
+        {
+            PopupNavigation.Instance.PushAsync(new SonPage());
+        }
+    }
+}
---
+++ SlimeMVVM/SlimeMVVM/Views/SonPage.xaml.cs
@@ -0,0 +1,27 @@
+﻿using Rg.Plugins.Popup.Pages;
+using Rg.Plugins.Popup.Services;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+using Xamarin.Forms;
+using Xamarin.Forms.Xaml;
+
+namespace SlimeMVVM.Views
+{
+    [XamlCompilation(XamlCompilationOptions.Compile)]
+    public partial class SonPage : PopupPage
+    {
+        public SonPage()
+        {
+            InitializeComponent();
+        }
+
+        private void Button_Clicked(object sender, EventArgs e)
+        {
+            PopupNavigation.Instance.PopAsync();
+        }
+    }
+}
